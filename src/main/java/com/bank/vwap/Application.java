2c51e90a151dd6c851ae1b417fe752f5c838f925
Aneesh <aneesh@sky.com>
2025-03-29 16:06:55 +1100

    Move instant creaton away from priceProcessor

--- src/main/java/com/bank/vwap/Application.java
+++ src/main/java/com/bank/vwap/Application.java
@@ -1,19 +1,22 @@
 package com.bank.vwap;
 
+import java.time.Instant;
+
 public class Application {
 
     public static void main(String[] args) {
 
         VWAPCalculator calculator = new VWAPCalculator();
         try {
-            calculator.processPriceUpdate("AUD/USD", 0.6905, 106198);
-            calculator.processPriceUpdate("USD/JPY", 142.497, 30995);
-            calculator.processPriceUpdate("USD/JPY", 139.392, 2890000);
-            calculator.processPriceUpdate("AUD/USD", 0.6899, 444134);
-            calculator.processPriceUpdate("NZD/GBP", 0.4731, 64380);
-            calculator.processPriceUpdate("NZD/GBP", 0.4725, 8226295);
+            calculator.processPriceUpdate(Instant.now(),"AUD/USD", 0.6905, 106198);
+            calculator.processPriceUpdate(Instant.now(),"USD/JPY", 142.497, 30995);
+            calculator.processPriceUpdate(Instant.now(),"USD/JPY", 139.392, 2890000);
+            calculator.processPriceUpdate(Instant.now(),"AUD/USD", 0.6899, 444134);
+            calculator.processPriceUpdate(Instant.now(),"NZD/GBP", 0.4731, 64380);
+            calculator.processPriceUpdate(Instant.now(),"NZD/GBP", 0.4725, 8226295);
         }
-        finally {            //close down ExecutorServices
+        finally {
+            //close down ExecutorServices
             calculator.shutdown();
         }
     }
